595c98c7b4a3744aaae29cb3efb41786feb8392c
Francisco Javier Sarasúa Galán <franciscoj.sarasua@luceit.es>
2022-03-21 20:56:15 +0100

    Initial Commit

---
+++ IUD_AKA_CRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IUD_AKA_CRUD
+//
+//  Created by Francisco Javier Sarasua Galan on 21/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
